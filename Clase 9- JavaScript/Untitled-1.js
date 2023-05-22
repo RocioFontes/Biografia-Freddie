a1ae76e55baebb82e7b376cfaf4488b01d72e9a0
Rocio Fontes <fontesrocio2002@gmail.com>
2023-05-22 18:18:26 -0300

    Java practica

---
+++ Clase 9- JavaScript/Untitled-1.js
@@ -0,0 +1,74 @@
+//Variables
+
+var nombre = "Anyi";
+console.log (nombre);
+
+nombre = "Lucas";
+console.log (nombre);
+
+var num = 1;
+console.log (num);
+num = 2;
+console.log (num);
+
+var suma = 20+20;
+console.log (suma);
+
+var num1 = 8; 
+var num2 = 9;
+suma = num1 + num2;
+console.log (suma);
+
+console.log (10+5*8);
+console.log ((10+5)*8);
+
+var bool = true;
+console.log (bool);
+
+//Constantes
+
+const nom = "Candela";
+console.log (nom);
+
+const numero1 = 100;
+const numero2 = "200";
+const numero3 = false;
+console.log (typeof numero1);
+console.log (typeof numero2);
+console.log (typeof numero3);
+
+const n1 = new Number (7);
+console.log (n1);
+
+const n2 = 4**4;
+console.log (n2);
+
+const n3 = 10 % 3;
+console.log (n3); 
+
+//Operador asignación
+
+const a = 3;
+const b = "3";
+
+if(a == b){
+    console.log ("A y B son iguales");
+}
+
+if (a === b){
+    console.log ("A y B son iguales")
+}else{
+    console.log ("A y B no son iguales");
+}
+
+//Cadenas
+
+const var1 = "Gabriel";
+const var2 = "Horacio";
+const var3 = var1 + " " + var2;
+console.log (var3);
+console.log (var1 + " " + var2);
+
+
+
+
